c45d30c9892f68559985258d134126d3bf0b2b96
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2023-07-19 17:01:28 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-import { AppContext } from "./context/contextApi";
+import  {AppContext}  from "./context/contextAPI";
 import "./App.css";
 import Feed from "./components/Feed";
 import SearchResult from "./components/SearchResult";
